a9a7cb11b9cf563270c21e5cdce7961dc0b8c31d
Mike Smith <gooseisdead@mac.com>
2020-08-29 16:44:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,32 @@
-# Write your methods here
+def loop_message_five_times(sring)
+  counter = 0
+  while counter < 5
+    puts "Hello World."
+    counter += 1
+  end
+end
+
+def loop_message_n_times(sring, limit)
+  counter = 0
+  while counter < 5
+    puts "Hello Moon."
+    counter += 1
+  end
+  counter = 0
+  while counter < 10
+    puts "Hello Red Balloon."
+    counter += 1
+  end
+end
+
+def output_array(array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"])
+  counter = 0
+  while counter < array.length
+    puts array
+    counter += 1
+  end
+end
+
+def return_string_array(array = [5, 4, 3, 2, 1])
+    puts array.to_s
+  end
